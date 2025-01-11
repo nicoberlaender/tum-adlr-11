b04d9ab0e1bb92dadf09d9067c0acbd9f29c8165
nicoberlaender <nicoberlaender@gmail.com>
2025-01-11 15:02:33 +0100

    small wandb changes

--- Reinforcement_Learning/ppo_train_hit_objects.py
+++ Reinforcement_Learning/ppo_train_hit_objects.py
@@ -2,10 +2,10 @@ import wandb
 from stable_baselines3 import PPO
 from test_environment import TestEnvironment
 from wandb.integration.sb3 import WandbCallback
-from gymnasium.wrappers import RecordEpisodeStatistics
+from stable_baselines3.common.monitor import Monitor
 
 env = TestEnvironment((224, 224), 15, "../data_new")
-env = RecordEpisodeStatistics(env)
+env = Monitor(env)
 
 config = {
     "env": env,
@@ -13,12 +13,14 @@ config = {
     "policy": "MlpPolicy"
 }
 
-run = wandb.init(project='ppo_hit_objects', config=config, sync_tensorboard=True, monitor_gym=True)
+run = wandb.init(project='ppo_hit_objects', config=config, sync_tensorboard=True, monitor_gym=True, save_code=True)
 
 model = PPO(config["policy"], config["env"], learning_rate=1e-3, verbose=1, tensorboard_log=f"runs/{run.id}")
 
 callback = WandbCallback(
     gradient_save_freq=1000,
+    model_save_freq=10000,
+    model_save_path=f"models/{run.id}",
     verbose=2
 )
 
