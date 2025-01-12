ba9794a787d870fc89783572cf83eeb70adad93f
IacopoDiPippo <dipippo.1966046@studenti.uniroma1.it>
2025-01-12 23:35:02 +0100

    s

--- Reinforcement_Learning/ppo_train_hit_objects.py
+++ Reinforcement_Learning/ppo_train_hit_objects.py
@@ -1,7 +1,7 @@
 import wandb
 from stable_baselines3 import PPO
-from Reinforcement_Learning.test_environment import TestEnvironment
-from Reinforcement_Learning.test_env2 import TestEnvironment2
+from test_environment import TestEnvironment
+from test_env2 import TestEnvironment2
 from wandb.integration.sb3 import WandbCallback
 from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
 import sys
