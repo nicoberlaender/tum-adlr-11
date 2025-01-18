6c09b259ff6c921e74347c3adac29429069c14b3
IacopoDiPippo <dipippo.1966046@studenti.uniroma1.it>
2025-01-18 12:10:35 +0100

    s

--- Reinforcement_Learning/ppo_train_hit_objects.py
+++ Reinforcement_Learning/ppo_train_hit_objects.py
@@ -44,7 +44,7 @@ progress_bar_callback = ProgressBarCallback()
 # Configure training
 config = {
     "env": env,
-    "total_timesteps": 40,
+    "total_timesteps": 400000,
     "policy": "MlpPolicy"
 }
 
